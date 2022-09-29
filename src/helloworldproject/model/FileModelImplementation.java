d10a5fa4f3314378bcda074c438b1285f8328184
HaizeaF <haize@DESKTOP-8QPK9LI>
2022-09-29 17:53:13 +0200

    Progress: FileModelImplementation

--- src/helloworldproject/model/FileModelImplementation.java
+++ src/helloworldproject/model/FileModelImplementation.java
@@ -1,15 +1,6 @@
 package helloworldproject.model;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * This class is the implementation of the model with a file. This class implements the model interface.
