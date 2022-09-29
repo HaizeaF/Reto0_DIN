c62ab637a62f5aac4af2708a40e2007c283522e5
HaizeaF <haize@DESKTOP-8QPK9LI>
2022-09-29 13:55:28 +0200

    Progress: FileModelImplementation

--- src/helloworldproject/model/FileModelImplementation.java
+++ src/helloworldproject/model/FileModelImplementation.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,25 +16,14 @@ import java.util.logging.Logger;
  * @author haize
  */
 public class FileModelImplementation implements Model {
-    File file = new File("src/helloworldproject/model/greeting.obj");
-    BufferedReader br = null;
-    String greeting = null;
-    
+     ResourceBundle bundle = ResourceBundle.getBundle("helloworldproject.config");
      /**
      * This method gets the greeting from the file using BufferedReader.
      * @return greeting obtained from the file.
      */
     @Override
     public String getGreeting() {
-        try {
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(file),StandardCharsets.UTF_8));
-            greeting = br.readLine();
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(FileModelImplementation.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(FileModelImplementation.class.getName()).log(Level.SEVERE, null, ex);
-        }
-       return greeting;
+       return bundle.getString("GREET");
     }
     
 }
